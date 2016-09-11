4426a810f6b2bf88730d86b55e9dbae63dbc3b4c
TSITCL\s162196 <dqx_2527@yahoo.co.jp>
2016-09-11 14:50:16 +0900

    敵の処理

---
+++ President_Legend/Assets/Kai/Assets/Scripts/StarScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class StarScript : MonoBehaviour
+{
+    void Start()
+    {
+        Sequence _myseq = DOTween.Sequence();
+        _myseq.Append(transform.DOLocalMoveX(transform.position.x - 2.0f, 2));
+        _myseq.Join(transform.DOLocalMoveY(transform.position.y - 2.0f, 2));
+        _myseq.Join(transform.DOScale(0, 2));
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        this.transform.Rotate(new Vector3(0.0f, 0.0f, 80.0f));
+        if (this.transform.localScale.x <= 0) 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
